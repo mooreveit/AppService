6104f0b79516f653c5a46fee5e4dcaec492dc2ff
reveronfranklin <reveronfranklin@gmail.com>
2022-10-07 13:26:24 -0400

    Update opuesta en clases de resumen de repeticiones

--- AppService.Core/Entities/AppRepeticionClientePapelQuintaParte.cs
+++ AppService.Core/Entities/AppRepeticionClientePapelQuintaParte.cs
@@ -8,6 +8,7 @@
         public string NombreProducto { get; set; }
         public string NombreForma { get; set; }
         public string BasicaHumano { get; set; }
+        public string OpuestaHumano { get; set; }
         public int partes_formula { get; set; }
         public int Cant_Tintas { get; set; }
         public string PAPELPRIMERAPARTE { get; set; }
