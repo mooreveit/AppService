377ef61c93c76e257fda873702af6bd630e1c80c
reveronfranklin <reveronfranklin@gmail.com>
2022-10-07 13:31:07 -0400

    Add opuesta en clientes parte

--- AppService.Core/Entities/AppRepeticionClientePartes.cs
+++ AppService.Core/Entities/AppRepeticionClientePartes.cs
@@ -7,6 +7,7 @@
         public string NombreProducto { get; set; }
         public string NombreForma { get; set; }
         public string BasicaHumano { get; set; }
+        public string OpuestaHumano { get; set; }
         public int partes_formula { get; set; }
 
     }
